81d673f4a5b6221fd0e73c2b672b0324ad747a24
NoLongerLucky <endroo04@gmail.com>
2022-10-26 15:08:23 -0400

    EditConfiguration's room display dynamically updates with configuration
    Also fixed a bug where config would not be updated when returning from EditConfigurationInfo

--- app/src/main/java/com/example/speakerbalancer/activity/EditConfiguration.java
+++ app/src/main/java/com/example/speakerbalancer/activity/EditConfiguration.java
@@ -6,42 +6,71 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.speakerbalancer.R;
 import com.example.speakerbalancer.data.AppDatabase;
 import com.example.speakerbalancer.data.StoredConfig;
 
 public class EditConfiguration extends AppCompatActivity {
-    int id;
+    int id, borderHeight, borderWidth, borderOriginal;
+    StoredConfig config;
     TextView name, systemType, roomLength, roomWidth, wallType;
+    ConstraintLayout border;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_configuration);
         id = getIntent().getExtras().getInt("id");
-        setData();
+        config = AppDatabase.getDatabase(getApplicationContext()).getDao().getData(id);
 
         name = findViewById(R.id.name);
         systemType = findViewById(R.id.systemType);
         roomLength = findViewById(R.id.roomLength);
         roomWidth = findViewById(R.id.roomWidth);
         wallType = findViewById(R.id.wallType);
+
+        border = findViewById(R.id.border);
+        border.post(() -> {
+            borderHeight = borderWidth = borderOriginal = border.getMeasuredHeight();
+            setRoom();
+        });
+
+        setData();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        config = AppDatabase.getDatabase(getApplicationContext()).getDao().getData(id);
         setData();
+        setRoom();
     }
 
     private void setData() {
-        StoredConfig config = AppDatabase.getDatabase(getApplicationContext()).getDao().getData(id);
         if (name != null) name.setText(config.getName());
         if (systemType != null) systemType.setText(config.getSystemType());
         if (roomLength != null) roomLength.setText(String.valueOf(config.getRoomLength()));
         if (roomWidth != null) roomWidth.setText(String.valueOf(config.getRoomWidth()));
         if (wallType != null) wallType.setText(config.getWallType());
+    }
+
+    private void setRoom() {
+        double length, width, denominator;
+        borderHeight = borderWidth = borderOriginal;
+        length = config.getRoomLength();
+        width = config.getRoomWidth();
+        if (length > width) {
+            denominator = length / width;
+            borderWidth /= denominator;
+        } else if (width > length) {
+            denominator = width / length;
+            borderHeight /= denominator;
+        }
+        border.getLayoutParams().height = borderHeight;
+        border.getLayoutParams().width = borderWidth;
+        border.setLayoutParams(border.getLayoutParams());
     }
 
     public void launch_editConfigInfo(View v) {
