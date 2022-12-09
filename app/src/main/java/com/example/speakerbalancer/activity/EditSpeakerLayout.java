61154346f9b42bb1eb729fe133b4cb09aa5bb9c7
NoLongerLucky <endroo04@gmail.com>
2022-12-09 15:28:41 -0500

    Set up SeekBar debug based on configured room size

--- app/src/main/java/com/example/speakerbalancer/activity/EditSpeakerLayout.java
+++ app/src/main/java/com/example/speakerbalancer/activity/EditSpeakerLayout.java
@@ -1,13 +1,60 @@
 package com.example.speakerbalancer.activity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.speakerbalancer.R;
 
 public class EditSpeakerLayout extends EditConfiguration {
+    SeekBar seekBarX, seekBarY;
+    TextView debug;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        seekBarX = findViewById(R.id.seekBarX);
+        seekBarY = findViewById(R.id.seekBarY);
+        debug = findViewById(R.id.debug);
+
+        seekBarX.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int num = config.getRoomWidth() * progress;
+                double text = (double)num / 100;
+                debug.setText(String.valueOf(text));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarY.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int num = config.getRoomLength() * progress;
+                double text = (double)num / 100;
+                debug.setText(String.valueOf(text));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     protected void setLayout() {
