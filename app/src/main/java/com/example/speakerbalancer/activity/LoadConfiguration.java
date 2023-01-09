72928efac45629d5f44389f3cfbb1816a607c6f0
NoLongerLucky <endroo04@gmail.com>
2023-01-08 22:15:10 -0500

    LoadConfiguration has text indicating that no configs have been created

--- app/src/main/java/com/example/speakerbalancer/activity/LoadConfiguration.java
+++ app/src/main/java/com/example/speakerbalancer/activity/LoadConfiguration.java
@@ -3,6 +3,8 @@ package com.example.speakerbalancer.activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,20 +20,29 @@ import java.util.List;
 
 public class LoadConfiguration extends AppCompatActivity {
     RecyclerView recyclerView;
+    TextView noConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_configuration);
         recyclerView = findViewById(R.id.recycler_view);
+        noConfig = findViewById(R.id.noConfig);
 
         getData();
+        updateTextVisibility();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         getData();
+    }
+
+    private void updateTextVisibility() {
+        int vis = View.INVISIBLE;
+        if (recyclerView.getAdapter() != null) if (recyclerView.getAdapter().getItemCount() == 0) vis = View.VISIBLE;
+        noConfig.setVisibility(vis);
     }
 
     private void getData() {
@@ -48,6 +59,7 @@ public class LoadConfiguration extends AppCompatActivity {
                     (dialog, which) -> {
                         AppDatabase.getDatabase(getApplicationContext()).getDao().deleteData(id);
                         getData();
+                        updateTextVisibility();
                         Toast.makeText(LoadConfiguration.this, getString(R.string.delYes), Toast.LENGTH_SHORT).show();
                     });
             alertDialogBuilder.setNegativeButton(getString(R.string.cancel),
